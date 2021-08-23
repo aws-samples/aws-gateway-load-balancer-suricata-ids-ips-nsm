ab86fe93ec9cc33ff0bd6f9bd5bfcffce4727f4d
Adam <adampalm@amazon.co.uk>
2021-08-23 17:55:01 +0100

    1. Modified Lua header script

--- Lua/script1.lua
+++ Lua/script1.lua
@@ -1,10 +1,19 @@
 function init (args)
     local needs = {}
-    needs["protocol"] = "http" 
+    needs["http.request_headers"] = tostring(true)
     return needs
 end
 
-http_ua = HttpGetRequestHeader("User-Agent")
-if http_ua == nil then
-    http_ua = "<useragent unknown>"
+function match(args)
+    a = tostring(args["http.request_headers"])
+    if #a > 0 then
+        if a:find("labheader: BlockThisValue") then
+            return 1
+        elseif a:find("labheader: AndThisValue") then
+            return 1
+        end
+    end
+    return 0
 end
+
+return 0
