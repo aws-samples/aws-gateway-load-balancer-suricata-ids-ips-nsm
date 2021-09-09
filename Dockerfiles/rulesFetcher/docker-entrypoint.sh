0d02284e82509a53441cbed8d1c1895d1f2445cb
Jesper Eneberg <jesper@eneberg.nu>
2021-09-09 10:40:27 +0200

    Update docker-entrypoint.sh

--- Dockerfiles/rulesFetcher/docker-entrypoint.sh
+++ Dockerfiles/rulesFetcher/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "0 0 * * WED su - suricata -s /bin/bash -c \"/maxmind-geodb-fetcher.sh cron $MaxMindApiKeySsmParameter $REGION\" >/proc/1/fd/1 2>/proc/1/fd/2" > /etc/cron.d/rules-updater.sh
+echo "0 0 * * WED su suricata -s /bin/bash -c \"/maxmind-geodb-fetcher.sh cron $MaxMindApiKeySsmParameter $REGION\" >/proc/1/fd/1 2>/proc/1/fd/2" > /etc/cron.d/rules-updater.sh
 crontab /etc/cron.d/rules-updater.sh
 
 exec /bin/supervisord -c /supervisord.conf --nodaemon
