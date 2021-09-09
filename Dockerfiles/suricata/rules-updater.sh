57f7db5a69eefe719de3965613b97e959588d69c
Jesper Eneberg <jesper@eneberg.nu>
2021-09-09 10:44:43 +0200

    Update rules-updater.sh

--- Dockerfiles/suricata/rules-updater.sh
+++ Dockerfiles/suricata/rules-updater.sh
@@ -4,7 +4,7 @@ do
 	fetched_rules_md5sum=$(find /var/lib/suricata/rules/ -type f -exec md5sum {} + | awk '{print $1}' | LC_ALL=C sort | md5sum | awk '{print $1}')
 	deployed_rules_md5sum=$(cat /tmp/rules.md5)
 	if [[ "$fetched_rules_md5sum" != "$deployed_rules_md5sum" ]] ; then 
-		echo "New rules found. Updating rules"
+		echo "[INFO] New rules found. Updating rules"
 		suricatasc -c reload-rules
 		echo "$fetched_rules_md5sum" > /tmp/rules.md5
 	fi
